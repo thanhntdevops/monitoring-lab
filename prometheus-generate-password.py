c8eb0ae21f40fda4c2b758e475eff7ea06cae8a7
thanhnt <thanhnguyenthe@vccorp.vn>
2023-09-19 10:04:17 +0700

    delete(node_exporter): delete the unused files

--- prometheus-generate-password.py
+++
@@ -1,7 +0,0 @@
-import getpass
-import bcrypt
-
-username = input("username: ")
-password = getpass.getpass("password: ")
-hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-print(username +": "+ hashed_password.decode())
